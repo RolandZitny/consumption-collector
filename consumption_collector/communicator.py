e42752a344a8742b9fdb091fbf1e0de27cbe5b95
RolandZitny <roland.zitny.it@gmail.com>
2022-07-20 15:23:33 +0200

    Minor updates

--- consumption_collector/communicator.py
+++ consumption_collector/communicator.py
@@ -99,9 +99,8 @@ class Communicator:
                      .field("M34", int(data[2]))
                      .field("M35", int(data[3]))
                      .field("M36", int(data[4]))
-                     .field("M37", int(data[5])))
-                     #  TODO v influxe neni na ms, len s
-                     # .time(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]))
+                     .field("M37", int(data[5]))
+                     .time(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]))
             self._collector.save_point(point)   # Save into Collector
 
 
